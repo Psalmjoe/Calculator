e70c87a6580cdea6f5f6b260dca1fab2e7b82471
Psalm Joe <terrafirma143@gmail.com>
2021-05-28 15:01:25 +0100

    added pseudocode

--- Desktop/index/index.js
+++ Desktop/index/index.js
@@ -1,13 +1,13 @@
-let str = prompt('Please enter amount of Money')
-let userNum = Number(str)
-console.log(userNum)
-function checkUser(userInput){
-    if(Object.is(userInput, NaN) || userNum <= 0){
+let string = prompt('State Money Value')
+let userNum = Number(string)
+
+function check(userEntry){
+    if(Object.is(userEntry, NaN) || userNum <= 0){
         alert('Please Enter number')
     }
     return userInput
 }
-let number = checkUser(userNum);
+let number = check(userNum);
 function getAmount(amount){
     let nairaSymbol = '#'
     if(amount <= 5000){
@@ -18,4 +18,17 @@ function getAmount(amount){
     return nairaSymbol + 50
 }
 
-console.log(getAmount(number));
+alert(getAmount(number));
+
+
+// pseudocode for commission calculator 
+
+// use function to get user input.
+// convert to number with built in function
+// write function to check the validity of user input
+// check if the number function return NaN
+// if it returns NaN, request user to enter number
+// else call the function and store in a variable
+// the above variable will be used to call another function which checks if the amount is less than or equal to 5000 then the return is 10
+// if the amount is greater than 5000 and less than or equal 50000 then the return is 25
+// if above 50000 it returns 50
